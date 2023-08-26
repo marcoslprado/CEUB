11d1b37d2e3bb145e5845d58b7a8d23c7b7735a7
Hu0no <126472671+Hu0no@users.noreply.github.com>
2023-08-26 10:40:43 -0300

    Update homework.py

--- 2 Semestre/Linguagens e técnicas de programação/Aula4/homework.py
+++ 2 Semestre/Linguagens e técnicas de programação/Aula4/homework.py
@@ -1,24 +1,17 @@
- # Programa que insere elementos em uma lista usando função recursiva #
-def IncreaseOnList(i,y,list): # Função recursiva que insere elementos na lista chamando ela mesma de acordo com o nº de elementos que o usuário deseja inserir #
-    while i < y:
-        try:
-            x = int(input(f"Digite o {i+1}º número a ser inserido na lista: "))
-            i+=1
-            list.append(x)
-            IncreaseOnList(i,y,list)
-        except:
-            print("Você deve digitar um número.\n")
-            print("=======================================\n")
-            IncreaseOnList(i,y,list)
-        return list
-list = []
-i = 0
-boolean = True
-while boolean: # Loop para utilização de tratamento de erro
-    try:
-        y = int(input("Quantos números você deseja incluir na lista? "))
-        boolean = False
+#  Programa que soma os elementos de uma lista utilizando função recursiva #
+# Declaração de variáveis #
+list = [] 
+
+def createList(): # Função que insere elementos na lista #
+    try: # Tratamento de erro #
+        x = float(input("Digite o número que você deseja inserir na lista: (Para parar de inserir digite 0) "))
+        if x == 0: # Quando o usuário digitar 0, encerra a função #
+            return
+        list.append(x) # Adiciona o número digitado à lista #
+        createList()
     except:
-        print("Você deve digitar um número maior que zero.")
-        print("=======================================\n")
-print("Sua lista resultante é: ",IncreaseOnList(i,y,list))
+        print("Você deve digitar um número. Tente novamente")
+        createList()
+
+createList()
+print("O resultado da soma dos elementos da lista é: ", sum(list))
