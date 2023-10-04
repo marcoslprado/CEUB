e580dd5e3fd630da4fe76ec1b2034b3e0262c712
Marcos de Lima Prado <126472671+marcoslprado@users.noreply.github.com>
2023-10-04 12:18:40 -0300

    Add files via upload

---
+++ 2 Semestre/MiniCursoC/Trabalho.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+struct conta {
+    char nomeCliente[100];
+    int numeroConta;
+    float saldo;
+};
+
+void Cadastro() {
+
+}
+
+void Busca() {
+
+}
+
+void Exclusao() {
+
+}
+
+void main() {
+    int opcao;
+    printf("------------------------BANCO------------------------\n");
+    printf("=====================================================\n");
+    printf("1. Cadastrar Conta\n");
+    printf("2. Visualizar todas as contas de determinado cliente\n");
+    printf("3. Excluir a conta com menor saldo\n");
+    printf("4. Sair\n");
+    printf("=====================================================\n");
+    scanf("%d", &opcao);
+    switch (opcao) {
+    case 1:
+        Cadastro();
+    case 2:
+        Busca();
+    case 3:
+        Exclusao();
+    case 4:
+
+    }
+}
