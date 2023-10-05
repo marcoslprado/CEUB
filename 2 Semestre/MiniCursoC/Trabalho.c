073714bb120bff3f80d5a1d74bdb613e7c34671f
Marcos de Lima Prado <126472671+marcoslprado@users.noreply.github.com>
2023-10-05 10:46:27 -0300

    Update Trabalho.c

--- 2 Semestre/MiniCursoC/Trabalho.c
+++ 2 Semestre/MiniCursoC/Trabalho.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-struct conta {
+struct Conta {
     char nomeCliente[100];
     int numeroConta;
     float saldo;
 };
 
 void Cadastro() {
-
 }
 
 void Busca() {
@@ -20,13 +19,14 @@ void Exclusao() {
 
 void main() {
     int opcao;
-    printf("------------------------BANCO------------------------\n");
-    printf("=====================================================\n");
-    printf("1. Cadastrar Conta\n");
-    printf("2. Visualizar todas as contas de determinado cliente\n");
-    printf("3. Excluir a conta com menor saldo\n");
-    printf("4. Sair\n");
-    printf("=====================================================\n");
+    printf("|=====================================================|\n");
+    printf("|------------------------BANCO------------------------|\n");
+    printf("|=====================================================|\n");
+    printf("|1. Cadastrar Conta                                   |\n");
+    printf("|2. Visualizar todas as contas de determinado cliente |\n");
+    printf("|3. Excluir a conta com menor saldo                   |\n");
+    printf("|4. Sair                                              |\n");
+    printf("|=====================================================|\n");
     scanf("%d", &opcao);
     switch (opcao) {
     case 1:
@@ -36,6 +36,6 @@ void main() {
     case 3:
         Exclusao();
     case 4:
-
+        break;
     }
 }
