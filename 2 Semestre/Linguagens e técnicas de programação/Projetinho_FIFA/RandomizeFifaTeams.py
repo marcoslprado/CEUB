b8390aa81766ca71e99912f37befac6a3d42fb35
Marcos de Lima Prado <126472671+marcoslprado@users.noreply.github.com>
2023-10-09 10:49:06 -0300

    Update RandomizeFifaTeams.py

--- 2 Semestre/Linguagens e técnicas de programação/Projetinho_FIFA/RandomizeFifaTeams.py
+++ 2 Semestre/Linguagens e técnicas de programação/Projetinho_FIFA/RandomizeFifaTeams.py
@@ -1,16 +1,52 @@
 # Programa que randomiza um time para jogar no FIFA #
-from random import randint
+import random
+import sys
+import time
+import os
 
-def DiscoverUserPreferencies():
-    try:
-        choice = int(input("Digite 0 para randomizar um time completamente aleatório ou 1 para randomizar um dos melhores times das ligas mais conhecidas: "))
-        if choice !=0 or choice != 1:
-            print("Você deve digitar um número sendo ele 0 ou 1! Tente novamente.")
-            DiscoverUserPreferencies()
-    except:
-        print("Você deve inserir um número sendo ele 0 ou 1! Tente novamente.")
-        DiscoverUserPreferencies()
-DiscoverUserPreferencies()
+def Random(): 
+    lista = [
+        "Argentinos Juniors", "Boca Juniors", "Estudiantes", "Independiente", "Old Boys", "Racing", "River Plate", "San Lorenzo", "Rosario Central", "Vélez",
+        "Club Brugge"
+        "Arsenal", "Aston Villa", "Brighton", "Chelsea", "Everton", "Crystal Palace", "Liverpool", "Manchester City", "Manchester United", "Newcastle", "Tottenham", "West Ham", "Wolves",
+        "Monaco", "Lyon", "Olympique de Marselha", "PSG", "Nantes", "Lille", "Nice",
+        "Borussia Dortmund", "Bayern de Munique", "Bayer Leverkusen", "RB Leipzig",
+        "Juventus", "Fiorentina", "Inter de Milão", "Lazio", "Milan", "Napoli", "Roma",
+        "Argentina", "Bélgica", "Inglaterra", "França", "Alemanha", "Itália", "México", "Holanda", "Polônia", "Portugal", "Espanha",
+        "Ajax", "AZ", "PSV",
+        "Porto", "Benfica","Sporting",
+        "AEK Atenas", "Dinamo Zagreb", "Dynamo Kyev", "Olympiacos", "Panathinaikos", "Shakhtar", "Spartak Moscou", "Viktoria Pilsen", "adidas All-Star", "Soccer Aid",
+        "Atlético Bilbao", "Atlético de Madrid", "Barcelona", "Real Madrid", "Sevilla", "Valencia", "Villarreal", "Real Sociedad", "Real Betis",
+        "Besiktas", "Fenerbahce", "Galatasaray"
+    ]
+    random_team = random.choice(lista)
+    print("Sorteando time...")
+    time.sleep(2)
+    print("Time sorteado: ", random_team)
+    time.sleep(3)
+    os.system('cls')
+    
 
-def Randomize(choice):
-    league = randint()
+
+
+def Choice():
+    while True:
+        print(f"|======================================================|")
+        print(f"|--------------SORTEADOR DE TIMES DO FIFA--------------|")
+        print(f"|======================================================|")
+        print(f"| 1. Sortear um time                                   |")
+        print(f"| 2. Sair                                              |")
+        print(f"|======================================================|")
+        try:
+            choice = int(input("Escolha uma opção: "))
+            if choice == 1:
+                Random()
+            elif choice == 2:
+                sys.exit()
+            else:
+                print("Você deve digitar 1 ou 2! Tente novamente.")
+        except ValueError:
+            print("Você deve inserir um número! Tente novamente.")
+
+
+Choice()
