380a7566d86dbd73cd322a38a21d678f46826c9e
Marcos de Lima Prado <126472671+marcoslprado@users.noreply.github.com>
2023-10-27 22:06:33 -0300

    Update and rename ex02.java to Ex02.java

--- 2 Semestre/MiniCursoJava/Aula3/Ex02.java
+++ 2 Semestre/MiniCursoJava/Aula3/Ex02.java
@@ -32,9 +32,9 @@ public class ex02 {
 		System.out.println("Digite o segundo numero: ");
 		num2 = sc.nextInt();
 		System.out.printf("a) Soma dos numeros: %.2f\n", A(num1,num2));
-		System.out.printf("b) O produto do primeiro número pelo quadrado do segundo: %.2f\n", B(num1,num2));
-		System.out.printf("c) O quadrado do primeiro número: %.2f\n", C(num1));
-		System.out.printf("d) O módulo do primeiro número: %.2f", D(num1));
+		System.out.printf("b) O produto do primeiro nÃºmero pelo quadrado do segundo: %.2f\n", B(num1,num2));
+		System.out.printf("c) O quadrado do primeiro nÃºmero: %.2f\n", C(num1));
+		System.out.printf("d) O mÃ³dulo do primeiro nÃºmero: %.2f", D(num1));
 	}
 
 }
