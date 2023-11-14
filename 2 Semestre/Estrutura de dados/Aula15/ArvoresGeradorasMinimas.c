8878dcc425cf2fbdf5e12227321362d7ca6af188
Marcos de Lima Prado <126472671+marcoslprado@users.noreply.github.com>
2023-11-14 08:45:00 -0300

    Add files via upload

---
+++ 2 Semestre/Estrutura de dados/Aula15/ArvoresGeradorasMinimas.c
@@ -0,0 +1,4 @@
+// Arvores Geradoras Minimas //
+// Grafo nao direcionado conectado com pesos positivos nas arestas //
+// Objetivo: Encontrar o conjunto de arestas com peso total minimo que conecte todos os vertices do grafo //
+
