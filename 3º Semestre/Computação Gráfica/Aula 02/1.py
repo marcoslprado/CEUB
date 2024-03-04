f7533735683462a8d272cc2e8f5a75c9e99d4158
Marcos de Lima Prado <126472671+marcoslprado@users.noreply.github.com>
2024-03-04 10:51:53 -0300

    Add files via upload

---
+++ 3º Semestre/Computação Gráfica/Aula 02/1.py
@@ -0,0 +1,14 @@
+import turtle as tr
+
+tr.color(0/255,255/255,242/255)
+tr.pensize(5)
+tr.begin_fill()
+
+tr.forward(100)
+tr.left(120)
+tr.forward(100)
+tr.left(120)
+tr.forward(100)
+tr.color(0/255,0/255,0/255)
+
+tr.end_fill()
