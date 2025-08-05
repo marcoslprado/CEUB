a5f7646911125d527f5e7605b6b114833abd8d4f
Marcos de Lima Prado <126472671+marcoslprado@users.noreply.github.com>
2025-08-05 20:32:14 -0300

    Create 02.py

---
+++ 6 Semestre/Teoria de Grafos/Aula01/02.py
@@ -0,0 +1,17 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# Criar um grafo vazio
+DG = nx.DiGraph()
+
+# Adicionar vértices (nós)
+DG.add_nodes_from(["A", "B", "C", "D"])
+
+# Adicionar arestas (conexões)
+DG.add_edges_from([("A", "B"), ("B", "C"), ("C", "A"), ("C", "D")])
+
+# Desenhar grafo
+plt.figure(figsize=(6,4))
+nx.draw(DG, with_labels=True, node_color='lightblue', node_size=1000, edge_color='gray', font_size=14)
+plt.title("Grafo Direcionado")
+plt.show()
